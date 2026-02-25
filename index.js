6058be42ede7c55951b3ae9c09bee167eea327c9
Mamoun Zaid <mam.j.zeid@gmail.com>
2026-02-25 16:57:00 +0300

    done

--- index.js
+++ index.js
@@ -17,7 +17,60 @@ const allWagesFor = function () {
     const payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
     return payable
+}
+
+function createEmployeeRecord(arr){
+    let obj = {}
+    obj.firstName = arr.at(0)
+    obj.familyName = arr.at(1)
+    obj.title = arr.at(2)
+    obj.payPerHour = arr.at(3)
+    obj.timeInEvents = []
+    obj.timeOutEvents = []
+    return obj
+}
+
+function createEmployeeRecords(arrOfArrays){
+    return arrOfArrays.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(dateStampSrting){
+    let [date, hour] = dateStampSrting.split(' ')
+    let timeInRecord = {}
+    timeInRecord.type = "TimeIn"
+    timeInRecord.hour = parseInt(hour, 10)
+    timeInRecord.date = date
+    this.timeInEvents.push(timeInRecord)
+    return this
+}
+function createTimeOutEvent(dateStampSrting){
+    let [date, hour] = dateStampSrting.split(' ')
+    let timeOutRecord = {}
+    timeOutRecord.type = "TimeOut"
+    timeOutRecord.hour = parseInt(hour, 10)
+    timeOutRecord.date = date
+    this.timeOutEvents.push(timeOutRecord)
+    return this
+}
+
+function hoursWorkedOnDate(dateStampSrting)
+{
+    let [date, hour] = dateStampSrting.split(' ')
+    return ((this.timeOutEvents.find(element => element.date == date).hour - this.timeInEvents.find(element => element.date == date).hour) / 100)
+}
+function wagesEarnedOnDate(dateStampSrting){
+    let houres = hoursWorkedOnDate.call(this, dateStampSrting)
+    let amounOwed = houres * this.payPerHour
+    return (amounOwed)
+}
+
+function findEmployeeByFirstName(collection, firstNameString){
+    return collection.find(element => element.firstName == firstNameString)
+}
+
+
+function calculatePayroll(employeeRecords){
+    return employeeRecords.reduce((acumm, element) => acumm += allWagesFor.call(element), 0)
 }
 
